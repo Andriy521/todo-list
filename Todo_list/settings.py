eccc670b995060da1aae82acb0df61c009138b4b
Andriy Hrudnyy <andgrudniy@gmail.com>
2025-04-03 19:33:00 +0200

    Added env for security

--- Todo_list/settings.py
+++ Todo_list/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +22,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-n8l%gr1##*(ni-8^y51+hg*g22$)(sezw*pfa%rw@#&$6yn8q5'
+load_dotenv()
+
+SECRET_KEY = os.getenv('SECRET_KEY')
+
+if not SECRET_KEY:
+    raise ValueError('SECRET_KEY is not set in the environment variables')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
