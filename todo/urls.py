cfebf7d68641b8419737c72e3435c220545416bb
Andriy Hrudnyy <andgrudniy@gmail.com>
2025-04-03 18:05:21 +0200

    Added task list, tag list, with all buttons

--- todo/urls.py
+++ todo/urls.py
@@ -1,8 +1,15 @@
 from django.urls import path
-from views import (
 
+from todo.views import (
+    TaskListView,
+    TagListView,
+    undo_task,
+    complete_task
 )
 
 urlpatterns = [
-    path('', views.index, name='index'),  # Головна сторінка
+    path('', TaskListView.as_view(), name='home'),
+    path('tags/', TagListView.as_view(), name='tags_list'),
+    path('complete/<int:task_id>/', complete_task, name='complete_task'),
+    path('undo/<int:task_id>/', undo_task, name='undo_task')
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views import generic
+from todo.models import Task, Tag
 
-# Create your views here.
+class TaskListView(generic.ListView):
+    model = Task
+    context_object_name = "tasks"
+    ordering = ["done_or_not", "-datetime"]
+    template_name = "todo/task_list.html"
+
+
+class TagListView(generic.ListView):
+    model = Tag
+    context_object_name = "tags"
+    ordering = ["name"]
+    template_name = "todo/tag_list.html"
+
+
+def complete_task(request, task_id):
+    task = Task.objects.get(id=task_id)
+    task.done_or_not = True
+    task.save()
+    return redirect('home')
+
+
+def undo_task(request, task_id):
+    task = Task.objects.get(id=task_id)
+    task.done_or_not = False
+    task.save()
+    return redirect('home')
+
+
